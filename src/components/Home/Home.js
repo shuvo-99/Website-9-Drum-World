4473aa6a025b54d2b98aeb2fb699aea4f9650536
shuvo-99 <anontoshuvo99@gmail.com>
2022-04-06 20:08:51 +0600

    home first template done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,16 +12,16 @@ const Home = () => {
         </div>
       </div> */}
       <Container>
-        <Row>
+        <Row className="justify-content-center align-items-center">
           <Col>
-            <h1>Welcome to drum world</h1>
+            <h1>Welcome to drum world!!</h1>
             <p>
               Find all kinds of drums and instument here all at once. We provide
-              the best drums to to enrich your music.
+              the best drums to enrich your music.
             </p>
           </Col>
           <Col>
-            <img src={drums} alt="" />
+            <img src={drums} className="img-fluid" alt="" />
           </Col>
         </Row>
       </Container>
